50e4c4248331d7b827a6122141eb1e34f4acc4f4
drfailer <drfailer.42@gmail.com>
2024-07-22 11:23:28 -0400

    update the parameter type
    For now, it will be used for the global program so all the information
    specific to the velocity have been removed.

--- Source/CPP/data/parameters.h
+++ Source/CPP/data/parameters.h
@@ -1,40 +1,21 @@
 #ifndef LOOP_FDS_PARAMETERS_H
 #define LOOP_FDS_PARAMETERS_H
-
-#include <cstddef>
 #include <memory>
+#include <string>
 
 struct Parameters {
-  Parameters(size_t ibp1, size_t jpb1, size_t kbp1, size_t iBlockSize, size_t jBlockSize,
-             size_t kBlockSize) :
-          ibp1(ibp1), jbp1(jpb1), kbp1(kbp1),
-          iBlockSize(iBlockSize),
-          jBlockSize(jBlockSize),
-          kBlockSize(kBlockSize),
-          // matrix: (0:IBPX) where IBPX = XBAR + 1
-          nbBlocksI((ibp1 + 1) / iBlockSize + (((ibp1 + 1) % iBlockSize == 0)? 0 : 1)),
-          nbBlocksJ((jpb1 + 1) / jBlockSize + (((jbp1 + 1) % jBlockSize == 0)? 0 : 1)),
-          nbBlocksK((kbp1 + 1) / kBlockSize + (((kbp1 + 1) % kBlockSize == 0)? 0 : 1)) {}
+  Parameters() : fnInput("null", 250) {}
 
-  Parameters(Parameters const &other):
-    Parameters(other.ibp1, other.jbp1, other.kbp1, other.iBlockSize, other.jBlockSize, other.kBlockSize) {}
+  Parameters(Parameters &&parameters)
+      : fnInput(std::move(parameters.fnInput)) {}
 
-  explicit Parameters(std::shared_ptr<Parameters> const &other):
-    Parameters(other->ibp1, other->jbp1, other->kbp1, other->iBlockSize, other->jBlockSize, other->kBlockSize) {}
+  Parameters(std::shared_ptr<Parameters> parameters)
+      : fnInput(parameters->fnInput) {}
 
-  explicit Parameters(std::shared_ptr<Parameters> &&other) noexcept :
-    Parameters(other->ibp1, other->jbp1, other->kbp1, other->iBlockSize, other->jBlockSize, other->kBlockSize) {}
+  Parameters(std::shared_ptr<Parameters> &&parameters)
+      : fnInput(std::move(parameters->fnInput)) {}
 
-  size_t ibp1 = 0;
-  size_t jbp1 = 0;
-  size_t kbp1 = 0;
-  size_t nedge = 0;
-  size_t iBlockSize = 0;
-  size_t jBlockSize = 0;
-  size_t kBlockSize = 0;
-  size_t nbBlocksI = 0;
-  size_t nbBlocksJ = 0;
-  size_t nbBlocksK = 0;
+  std::string fnInput = "";
 };
 
-#endif //LOOP_FDS_PARAMETERS_H
+#endif // LOOP_FDS_PARAMETERS_H
