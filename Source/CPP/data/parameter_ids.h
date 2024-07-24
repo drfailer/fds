da2b6dcc5fe97e9bd72b73233fefa2a7bd829e0d
drfailer <drfailer.42@gmail.com>
2024-07-24 11:33:05 -0400

    added parameter ids
    For now the parameters is the only data tha flows into the graph. These
    ids are used to control the data flow in the states. They should be
    removed / replace in the final version (we won't use parameters but
    matrix blocks)

---
+++ Source/CPP/data/parameter_ids.h
@@ -0,0 +1,15 @@
+#ifndef PARAMETER_IDS_H
+#define PARAMETER_IDS_H
+
+// WARN: this is a temporary file (parameters shouldn't flow into the final
+// graph)
+
+enum class ParameterIds {
+  None,
+  InstabilityCheck,
+  TimeStepReduced,
+  UpdateTS,
+  Finished,
+};
+
+#endif
